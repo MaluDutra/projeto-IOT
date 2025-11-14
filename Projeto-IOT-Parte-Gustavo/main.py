5850a4a6d331fdf76ffd6b7201e2adb1ad64dbad
gustans <gugab04@gmail.com>
2025-11-14 16:44:40 -0300

    add query verification

--- Projeto-IOT-Parte-Gustavo/main.py
+++ Projeto-IOT-Parte-Gustavo/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from dotenv import load_dotenv
 import psycopg2
 import os
@@ -6,6 +6,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = b'a secret key'
 
 conn = psycopg2.connect(
     database = "projeto_b", 
@@ -34,11 +35,18 @@ def alunos_add_post():
     nome = request.form["nome"]
     data = request.form["data_nascimento"]
     genero = request.form["genero"]
-    cur.execute('INSERT INTO arcondicionado_alunos (matricula, nome, data_nascimento, genero) VALUES (%s, %s, %s, %s);', (matricula, nome, data, genero))
+    try:
+        cur.execute('INSERT INTO arcondicionado_alunos (matricula, nome, data_nascimento, genero) VALUES (%s, %s, %s, %s);', (matricula, nome, data, genero))
+    except:
+        conn.commit()
+        cur.close()
+        flash("Erro ao cadastrar aluno", 'error')
+        return redirect("/")
     # Make the changes to the database persistent
     conn.commit()
     # Close cursor and communication with the database
     cur.close()
+    flash(f'Aluno cadastrado com sucesso', 'success')
     return redirect("/")
 
 @app.route('/alunos/get')
@@ -52,19 +60,39 @@ def alunos_get():
 
 @app.route('/alunos/edit', methods=['GET'])
 def alunos_edit_get():
-    return render_template('./alunos_edit.html')
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT matricula
+        FROM arcondicionado_alunos;""")
+    matriculas = cur.fetchall()
+    conn.commit()
+
+    cur.execute(
+        """SELECT aula, turma
+        FROM arcondicionado_aulas_horas;""")
+    aulas = cur.fetchall()
+    conn.commit()
+
+    return render_template('./alunos_edit.html', matriculas=matriculas, aulas=aulas)
 
 @app.route('/alunos/edit', methods=['POST'])
 def alunos_edit_post():
     cur = conn.cursor()
-    matricula = request.form["matricula"]
-    aula = request.form["aula"]
-    turma = request.form["turma"]
-    cur.execute('INSERT INTO arcondicionado_alunos_aulas (matricula, aula, turma) VALUES (%s, %s, %s);', (matricula, aula, turma))
+    matricula = request.form["matricula-option"]
+    aula = request.form["aula-option"]
+    turma = request.form["turma-option"]
+    try:
+        cur.execute('INSERT INTO arcondicionado_alunos_aulas (matricula, aula, turma) VALUES (%s, %s, %s);', (matricula, aula, turma))
+    except:
+        conn.commit()
+        cur.close()
+        flash("Aluno já matriculado", 'error')
+        return redirect("/")
     # Make the changes to the database persistent
     conn.commit()
     # Close cursor and communication with the database
     cur.close()
+    flash(f'Aluno matriculado com sucesso', 'success')
     return redirect("/")
 
 @app.route('/aulas')
@@ -80,28 +108,49 @@ def aulas_add_post():
     cur = conn.cursor()
     codigo = request.form["codigo"]
     nome = request.form["nome"]
-    cur.execute('INSERT INTO arcondicionado_aulas (codigo, nome) VALUES (%s, %s);', (codigo, nome))
+    try:
+        cur.execute('INSERT INTO arcondicionado_aulas (codigo, nome) VALUES (%s, %s);', (codigo, nome))
+    except:
+        conn.commit()
+        cur.close()
+        flash("Erro ao cadastrar aula", 'error')
+        return redirect("/")
     # Make the changes to the database persistent
     conn.commit()
     # Close cursor and communication with the database
     cur.close()
+    flash(f'Aula cadastrada com sucesso', 'success')
     return redirect("/")
 
 @app.route('/aulas/edit', methods=['GET'])
 def aulas_edit_get():
-    return render_template('./aulas_edit.html')
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT codigo
+        FROM arcondicionado_aulas;""")
+    aulas = cur.fetchall()
+    conn.commit()
+
+    return render_template('./aulas_edit.html', aulas=aulas)
 
 @app.route('/aulas/edit', methods=['POST'])
 def aulas_edit_post():
     cur = conn.cursor()
-    aula = request.form["aula"]
+    aula = request.form["aula-option"]
     turma = request.form["turma"]
     dia = request.form["dia"]
-    cur.execute('INSERT INTO arcondicionado_aulas_horas (aula, turma, dia) VALUES (%s, %s, %s);', (aula, turma, dia))
+    try:
+        cur.execute('INSERT INTO arcondicionado_aulas_horas (aula, turma, dia) VALUES (%s, %s, %s);', (aula, turma, dia))
+    except:
+        conn.commit()
+        cur.close()
+        flash("Turma já cadastrada", 'error')
+        return redirect("/")
     # Make the changes to the database persistent
     conn.commit()
     # Close cursor and communication with the database
     cur.close()
+    flash(f'Turma cadastrada com sucesso', 'success')
     return redirect("/")
 
 @app.route('/aulas/get')
@@ -135,11 +184,18 @@ def salas_add_post():
     cur = conn.cursor()
     codigo = request.form["codigo"]
     local = request.form["local"]
-    cur.execute('INSERT INTO arcondicionado_salas (codigo, local) VALUES (%s, %s);', (codigo, local))
+    try:
+        cur.execute('INSERT INTO arcondicionado_salas (codigo, local) VALUES (%s, %s);', (codigo, local))
+    except:
+        conn.commit()
+        cur.close()
+        flash("Erro ao cadastrar sala", 'error')
+        return redirect("/")
     # Make the changes to the database persistent
     conn.commit()
     # Close cursor and communication with the database
     cur.close()
+    flash(f'Sala cadastrada com sucesso', 'success')
     return redirect("/")
 
 @app.route('/salas/get')
@@ -157,18 +213,32 @@ def aparelhos():
 
 @app.route('/aparelhos/add', methods=['GET'])
 def aparelhos_add_get():
-    return render_template('./aparelhos_add.html')
+    cur = conn.cursor()
+    cur.execute(
+        """SELECT codigo
+        FROM arcondicionado_salas;""")
+    salas = cur.fetchall()
+    conn.commit()
+
+    return render_template('./aparelhos_add.html', salas=salas)
 
 @app.route('/aparelhos/add', methods=['POST'])
 def aparelhos_add_post():
     cur = conn.cursor()
     codigo = request.form["codigo"]
-    sala = request.form["sala"]
-    cur.execute('INSERT INTO arcondicionado_aparelhos (codigo, sala, qualidade) VALUES (%s, %s, %s);', (codigo, sala, "bom"))
+    sala = request.form["sala-option"]
+    try:
+        cur.execute('INSERT INTO arcondicionado_aparelhos (codigo, sala, qualidade) VALUES (%s, %s, %s);', (codigo, sala, "bom"))
+    except:
+        conn.commit()
+        cur.close()
+        flash("Aparelho já existe", 'error')
+        return redirect("/")
     # Make the changes to the database persistent
     conn.commit()
     # Close cursor and communication with the database
     cur.close()
+    flash(f'Aparelho cadastrado com sucesso', 'success')
     return redirect("/")
 
 @app.route('/aparelhos/get')
