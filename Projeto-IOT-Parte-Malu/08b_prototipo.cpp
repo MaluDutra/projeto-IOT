42f6ce487bb8b3fac2640af195a29c40f8e20163
Maria Luiza Dutra <132154059+MaluDutra@users.noreply.github.com>
2025-11-21 15:39:24 -0300

    versão legal

--- Projeto-IOT-Parte-Malu/08b_prototipo.cpp
+++ Projeto-IOT-Parte-Malu/08b_prototipo.cpp
@@ -4,77 +4,140 @@
 #include "certificados.h"
 #include <MQTT.h>
 #include <ArduinoJson.h>
+#include <IRremoteESP8266.h>
+#include <IRac.h>
+#include <IRutils.h>
+#include <string>
 
 WiFiClientSecure conexaoSegura;
 MQTTClient mqtt(1000);
 
-JsonDocument listaChatIds; // aqueles ainda não cadastrados
-JsonDocument listaAlunos;
-// lista de Jsons de alunos:
-// matricula do aluno
-// id do chat
-// se está alterando o dispositivo --> começa com false
-// se está em votação --> começa com false
+const uint16_t IRled = 4;    // pino do bagulho que envia
+IRac ar_condicionado(IRled); // classe que realiza o controle genérico do ar condicionado
 
-String procurarChatId(String chatId)
+int num_dispositivo = 1;
+
+enum class comando
 {
-  Serial.print("\nChat Id a ser procurado: ");
-  Serial.println(chatId);
+  Power,
+  Temperature,
+  OpMode,
+  FanSpeed
+};
 
-  for (unsigned int i = 0; i < listaChatIds.size(); i++)
-  {
-    if (chatId == listaChatIds[i])
-    {
-      Serial.println("Encontrou!\n");
-      return listaChatIds[i];
-    }
-    Serial.println(String(listaChatIds[i]));
-  }
+union tiposDeInstrucoes
+{
+  bool ligar;
+  float temperatura;
+  stdAc::opmode_t modo_operacao;
+  stdAc::fanspeed_t vel_ventilador;
+};
 
-  Serial.println("Não encontrado!\n");
-  return "";
+struct ACControle
+{
+  comando tipo;
+  tiposDeInstrucoes instrucao;
+};
+
+stdAc::state_t estadoDoAr;
+
+
+void setup_AC()
+{
+  ar_condicionado.next.protocol = decode_type_t::COOLIX; // Set a protocol to use.
+  ar_condicionado.next.model = 1;                        // Some A/Cs have different models. Try just the first.
+  ar_condicionado.next.mode = static_cast<stdAc::opmode_t>(1);
+  ar_condicionado.next.celsius = true;  // Use Celsius for temp units. False = Fahrenheit
+  ar_condicionado.next.degrees = 25.0f; // 25 degrees.
+  ar_condicionado.next.fanspeed = static_cast<stdAc::fanspeed_t>(3);
+  ar_condicionado.next.swingv = stdAc::swingv_t::kOff; // Don't swing the fan up or down.
+  ar_condicionado.next.swingh = stdAc::swingh_t::kOff; // Don't swing the fan left or right.
+  ar_condicionado.next.light = false;                  // Turn off any LED/Lights/Display that we can.
+  ar_condicionado.next.beep = false;                   // Turn off any beep from the A/C if we can.
+  ar_condicionado.next.econo = false;                  // Turn off any economy modes if we can.
+  ar_condicionado.next.filter = false;                 // Turn off any Ion/Mold/Health filters if we can.
+  ar_condicionado.next.turbo = false;                  // Don't use any turbo/powerful/etc modes.
+  ar_condicionado.next.quiet = false;                  // Don't use any quiet/silent/etc modes.
+  ar_condicionado.next.sleep = -1;                     // Don't set any sleep time or modes.
+  ar_condicionado.next.clean = false;                  // Turn off any Cleaning options if we can.
+  ar_condicionado.next.clock = -1;                     // Don't set any current time if we can avoid it.
+  ar_condicionado.next.power = true;                   // Initially start with the unit on
 }
 
-int procurarAlunoMatricula(String matricula)
+void envia_comando(ACControle *comando)
 {
-  Serial.print("\nMatrícula a ser procurada: ");
-  Serial.println(matricula);
-
-  for (unsigned int i = 0; i < listaAlunos.size(); i++)
+  decode_type_t protocolo = COOLIX;
+  if (ar_condicionado.isProtocolSupported(protocolo))
   {
-    JsonDocument aux = listaAlunos[i];
+    stdAc::state_t estado = ar_condicionado.next;
+    estado.protocol = protocolo;
 
-    if (matricula == aux[0])
+    switch (comando->tipo)
     {
-      Serial.println("Encontrou!\n");
-      return i;
+    case comando::Power:
+      estado.power = comando->instrucao.ligar;
+      break;
+    case comando::OpMode:
+      estado.mode = comando->instrucao.modo_operacao;
+      break;
+    case comando::Temperature:
+      estado.degrees = comando->instrucao.temperatura;
+      break;
+    case comando::FanSpeed:
+      estado.fanspeed = comando->instrucao.vel_ventilador;
+      break;
+    default:
+      Serial.println("Comando invalido!");
+      break;
     }
-    Serial.println(String(aux[0]));
-  }
 
-  Serial.println("Não encontrado!\n");
-  return -1;
+    Serial.println("Enviando comando a um ar condicionado " + typeToString(protocolo));
+    ar_condicionado.next = estado; // The state we want the device to be in after we send.
+    ar_condicionado.sendAc();      // Send an A/C message based soley on our internal state
+  }
 }
 
-int procurarAlunoChatId(String chatId)
+void imprimeEstadoDoAr(stdAc::state_t estado)
 {
-  Serial.print("\nChat Id a ser procurado: ");
-  Serial.println(chatId);
+  // Temperatura do ar
+  Serial.println("Temperatura do ar = " + String(estado.degrees, 1U) + " °C");
 
-  for (unsigned int i = 0; i < listaAlunos.size(); i++)
+  // Modo do ar
+  switch (estado.mode)
   {
-    JsonDocument aux = listaAlunos[i];
-
-    if (chatId == aux[1])
-    {
-      Serial.println("Encontrou!\n");
-      return i;
-    }
-    Serial.println(String(aux[1]));
+    using namespace stdAc;
+  case opmode_t::kAuto:
+    Serial.println("AUTO");
+    break;
+  case opmode_t::kCool:
+    Serial.println("COOL");
+    break;
+  case opmode_t::kDry:
+    Serial.println("SECAR");
+    break;
+  case opmode_t::kFan:
+    Serial.println("VENTILAR");
+    break;
+  case opmode_t::kHeat:
+    Serial.println("AQUECER");
+    break;
+  case opmode_t::kOff:
+    Serial.println("DESLIGADO");
+    break;
   }
 
-  Serial.println("Não encontrado!\n");
-  return -1;
+  // Velocidade do ventilador
+  Serial.println("Velocidade do ar = " + String(static_cast<int>(estado.fanspeed)));
+
+  // Power
+  if (estado.power)
+  {
+    Serial.println("LIGADO");
+  }
+  else
+  {
+    Serial.println("DESLIGADO");
+  }
 }
 
 void reconectarWiFi()
@@ -100,95 +163,37 @@ void reconectarMQTT()
     Serial.print("Conectando MQTT...");
     while (!mqtt.connected())
     {
-      mqtt.connect("8883", "aula", "zowmad-tavQez"); // verificar!!
+      mqtt.connect("8883", "aula", "zowmad-tavQez");!
       Serial.print(".");
       delay(1000);
     }
     Serial.println(" conectado!");
 
-    mqtt.subscribe("arcondicionado/telegram", 1); // verificar!!
+    mqtt.subscribe("arcondicionado/dispositivos/" + String(num_dispositivo), 1);
   }
-}
-
-// inicializar lista alunos --> percorrer o banco de dados, verificar aqueles que já tem chatId e adicioná-los à lista
-void inicializarListaAlunos()
-{
-
 }
 
 void recebeuMensagem(String topico, String conteudo)
 {
   Serial.println(topico + ": " + conteudo);
 
-  if (topico.startsWith("arcondicionado/telegram")) // verificar!!
+  if (topico.startsWith("arcondicionado/dispositivos/" + String(num_dispositivo))) 
   {
-    // dentro de conteúdo, preciso ter a mensagem e id do chat --> JSON
-    JsonDocument mensagem;
-    deserializeJson(mensagem, conteudo);
+    // recebeu mudança de temperatura!
+    Serial.print("Mudar para temperatura: ");
+    Serial.println(conteudo);
 
-    String chatId = mensagem["chatId"];
-    String conteudo_mensagem = mensagem["content"];
-    Serial.print("chatId: ");
-    Serial.println(chatId);
-    Serial.print("content: ");
-    Serial.println(conteudo_mensagem);
+    ACControle comandoDoAr;
+    comandoDoAr.tipo = comando::Temperature;
+    float nova_temperatura = conteudo.toFloat();
+    comandoDoAr.instrucao.temperatura = nova_temperatura;
 
-    if (conteudo_mensagem == "Iniciar Votação")
-    {
-      // verificar se já não há uma votação em andamento
-      // se já houver, pedir para aguardar o resultado
-      // senão:
-      // enviar mensagem para todos os alunos da sala do determinado aluno que iniciou a votação
-      // esperar a resposta deles por um tempo determinado
-      // verificar o resultado
-      // se a mudança ganhou, alterar a temperatura do ar enviando um sinal infravermelho
-      // senão, não fazer nada
-    }
-    else if (conteudo_mensagem == "Temperatura Atual")
-    {
-      // pegar qual é a sala atual em que o aluno está
-      // encontrar a temperatura da sala atual no banco de dados
-      // enviar mensagem dizendo a temperatura --> publish
-    }
-    else if (conteudo_mensagem == "Alterar Dispositivo")
-    {
-      // alterando_dispositivo = true;
-      // perguntar a matrícula
-      // remover o chat id relacionado ao aluno no banco de dados
-      // informar que ele precisará cadastrar novamente no novo dispositivo
-    }
-    else if (conteudo_mensagem.startsWith("Matrícula:"))
-    {
-      // if (alterando_dispositivo)
-      // {
-      //   // pegar a matrícula, acessar o banco de dados e remover o chat id dessa chave
-      //   alterando_dispositivo = false;
-      // }
-      // else if (cadastrando_dispositivo)
-      // {
-      //   // pegar a matrícula, acessar o banco de dados e adicionar o chat id nessa chave
-      //   cadastrando_dispositivo = false;
-      // }
-    }
-    else if (conteudo_mensagem == "Sim")
-    {
-    }
-    else if (conteudo_mensagem == "Não")
-    {
-    }
-    else
-    {
-      // verificar se o id da conversa já está cadastrado no banco de dados
-      if (procurarAlunoChatId(chatId) == -1)
-      {
-        // se não estiver cadastrado, perguntar a matrícula do aluno e cadastrar o id no banco
-        // cadastrando_dispositivo = true;
-      }
-      else
-      {
-        // se já tiver cadastrado, mostrar as opções de conversa
-      }
-    }
+    envia_comando(&comandoDoAr);
+
+    Serial.println("Novo estado do ar:");
+    estadoDoAr = ar_condicionado.getState(); // Para pegar o estado do ar
+    imprimeEstadoDoAr(estadoDoAr);
+    Serial.println("==============================================\n");
   }
 }
 
@@ -204,15 +209,58 @@ void setup()
   mqtt.begin("mqtt.janks.dev.br", 8883, conexaoSegura);
   mqtt.onMessage(recebeuMensagem);
   mqtt.setKeepAlive(10);
-  mqtt.setWill("arcondicionado/dispositivo", "Dispositivo desconectado");
+  
+  char topico_will[] = "arcondicionado/dispositivos/desconexao";
+  char mensagem_will[] = "1"; // num_dispositivo
+  mqtt.setWill(topico_will, mensagem_will);
   reconectarMQTT();
 
-  inicializarListaAlunos();
+  setup_AC();
+  Serial.println("Estado inicial do ar:");
+  estadoDoAr = ar_condicionado.next;
+  imprimeEstadoDoAr(estadoDoAr);
+  Serial.println("==============================================\n");
 }
 
+unsigned int instanteAnterior = 0;
+bool ligado = false;
 void loop()
 {
   reconectarWiFi();
   reconectarMQTT();
   mqtt.loop();
+
+  // unsigned long instanteAtual = millis();
+  // if (instanteAtual > instanteAnterior + 5000)
+  // {
+  //   Serial.println("+20 segundo");
+
+  //   estadoDoAr = ar_condicionado.getState(); // Para pegar o estado do ar
+  //   imprimeEstadoDoAr(estadoDoAr);
+
+  //   ACControle comandoDoAr;
+  //   // comandoDoAr.tipo = comando::Temperature;
+  //   // float minTemp = 17.0f;
+  //   // float maxTemp = 25.0f;
+  //   // comandoDoAr.instrucao.temperatura = minTemp + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (maxTemp - minTemp)));
+  //   comandoDoAr.tipo = comando::Power;
+  //   // comandoDoAr.instrucao.ligar = false;
+  //   // envia_comando(&comandoDoAr);
+
+  //   if (ligado)
+  //   {
+  //     Serial.println("Vai desligar!!");
+  //     comandoDoAr.instrucao.ligar = false;
+  //     ligado = false;
+  //   }
+  //   else
+  //   {
+  //     Serial.println("Vai ligar!!");
+  //     comandoDoAr.instrucao.ligar = true;
+  //     ligado = true;
+  //   }
+  //   envia_comando(&comandoDoAr);
+
+  //   instanteAnterior = instanteAtual;
+  // }
 }
